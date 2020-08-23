b4eb00e3aa298d943f65f4278c123492219c1525
Alex Mayorga <alexander.mayorga.m@gmail.com>
2020-08-22 19:33:26 -0500

    Sassify

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
 import { BrowserRouter } from 'react-router-dom';
-import './Resources/css/app.css'
+// import './Resources/css/app.css'
+import './Resources/sass/style.scss'
 import { firebase } from "./firebase";
 
 const App = (props) => {
